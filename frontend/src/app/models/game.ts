8f57c73446a1a3ea31cf31f1dd2d3cca8261303b
lucaznch <lucazanchisilva@gmail.com>
2025-06-13 15:14:31 +0100

    feat(frontend-model): add Question class

--- frontend/src/app/models/game.ts
+++ frontend/src/app/models/game.ts
@@ -1,6 +1,9 @@
+import { Question } from './question';
+
 export class Game {
     id!: number;
     name!: string;
     category!: string;
     level!: string;
+    questions!: Question[];
 }
---
+++ frontend/src/app/models/question.ts
@@ -0,0 +1,7 @@
+export class Question {
+    id!: number;
+    question!: string;
+    answer!: string;
+    category!: string;
+    level!: string;
+}
